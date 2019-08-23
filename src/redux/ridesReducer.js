2e10e599ded43c8fc83c69fc0f0d1252e05e5805
Kim Izatt <kimizatt@gmail.com>
2019-08-23 11:10:10 -0600

    DriverHistory Component

--- src/redux/ridesReducer.js
+++ src/redux/ridesReducer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_RIDES, SAVE_CRITERIA, GET_RIDES_BY_ID, CREATE_RIDE } from "./actionTypes.js";
+import { GET_RIDES, SAVE_CRITERIA, GET_RIDES_BY_ID, CREATE_RIDE, GET_RIDES_BY_DRIVER_ID} from "./actionTypes.js";
 
 const initialState = {
     searchCriteria: {
@@ -35,9 +35,9 @@ export const saveSearchCriteria = (firstDate, secondDate, location, locationLati
     }
 }
 
-export const getRidesById = (userId) => {
+export const getRidesById = (driverId) => {
     let data = axios
-        .get(`/api/get-rides-by-id/${userId}`)
+        .get(`/api/get-rides-by-id/${driverId}`)
         .then(res => {
             return res.data
         })
@@ -51,6 +51,19 @@ export const createRide = (request_date, request_location_lat, request_location_
     let data = axios.post("/api/create-request", { request_date, request_location_lat, request_location_long, request_seat_number, tier_id, rider_id, request_start_time, request_end_time }).then(res => res.data)
     return { type: CREATE_RIDE, payload: data };
 };
+
+export const getRidesByDriverId = (userId) => {
+    let data = axios
+        .get(`/api/get-rides-by-id/${userId}`)
+        .then(res => {
+            return res.data
+        })
+    return {
+        type: GET_RIDES_BY_DRIVER_ID,
+        payload: data,
+        error: false
+    }
+}
 
 
 //Action Function
@@ -67,10 +80,15 @@ export default function (state = initialState, action) {
             return { ...state, filteredRides: payload };
         case GET_RIDES_BY_ID + "_REJECTED":
             return { ...state, error: payload };
+        
         //DRIVER RIDES
         case CREATE_RIDE + "_FULFILLED":
             return { ...state, rides: payload };
         case CREATE_RIDE + "_REJECTED":
+            return { ...state, error: payload };
+        case GET_RIDES_BY_DRIVER_ID + "_FULFILLED":
+            return { ...state, filteredRides: payload };
+        case GET_RIDES_BY_DRIVER_ID + "_REJECTED":
             return { ...state, error: payload };
         default:
             console.log('Hit the default action type')
