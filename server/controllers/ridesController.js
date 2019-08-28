d5fc04ac6db2ba69fdc1bec826a942e9b5788832
Esteban Hernandez <contact@mksmdesign.com>
2019-08-28 14:23:49 -0600

    Fixed errors on request a ride

--- server/controllers/ridesController.js
+++ server/controllers/ridesController.js
@@ -14,21 +14,15 @@ module.exports = {
     
     getRidesByDriverId: async function (req, res) {
         let { driverId } = req.params;
-        console.log(driverId, 'driverId')
-        console.log('hits controller function')
         const db = req.app.get("db");
         let rides = await db.get_rides_by_driver_id(+driverId);
-        console.log(rides, 'rides')
         res.send(rides)
     },
 
     getConfirmedRidesByDriverId: async function (req, res) {
         let { driverId } = req.params;
-        console.log(driverId, 'driverId')
-        console.log('hits controller function')
         const db = req.app.get("db");
         let rides = await db.get_confirmed_rides_by_driver_id(+driverId);
-        console.log(rides, 'rides')
         res.send(rides)
     },
 
