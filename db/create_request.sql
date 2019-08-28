3aa8296fa57e3001913ed151e33a5aeb614baeef
Kim Izatt <kimizatt@gmail.com>
2019-08-28 12:15:17 -0600

    Twilio working

--- db/create_request.sql
+++ db/create_request.sql
@@ -1,3 +1,3 @@
-INSERT INTO requests(request_date, request_location_lat, request_location_long, request_seat_number, tier_id, rider_id, request_start_time, request_end_time)
-VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+INSERT INTO requests(request_date, request_location_lat, request_location_long, request_seat_number, tier_id, rider_id, request_start_time, request_end_time, requester_cell_number)
+VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
 RETURNING *;
