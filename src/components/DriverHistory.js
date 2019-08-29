b4f3cbe84ef72f754b46debc6eac8a34177fe805
Tori <toripetersen11@gmail.com>
2019-08-29 12:44:33 -0600

    styling driver upcoming rides and history

--- src/components/DriverHistory.js
+++ src/components/DriverHistory.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect, withRouter } from "react-router-dom";
-import {getRidesByDriverId} from "../redux/ridesReducer"
+import { getRidesByDriverId } from "../redux/ridesReducer"
 import DriverPastRides from './DriverPastRides.js'
+import "./coreStyling.css";
+import "./dashboardStyling.css";
 
-class DriverHistory extends Component{
+class DriverHistory extends Component {
 
     componentDidMount() {
-        let {id} = this.props.user.user
+        let { id } = this.props.user.user
         this.props.getRidesByDriverId(id)
     }
 
@@ -19,42 +21,66 @@ class DriverHistory extends Component{
     //     this.props.history.push('/driver-dashboard/create-a-ride')
     // }
 
-    render(){
+    render() {
         let { user } = this.props.user;
-        if(!user.loggedIn){
+        if (!user.loggedIn) {
             return <Redirect to="/" />
         }
-       
+
         console.log(this.props, "this.props")
-        let {filteredRides} = this.props.rides
-        
-        let ridesDisplay = filteredRides.filter( function (ride) {
+        let { filteredRides } = this.props.rides
+
+        let ridesDisplay = filteredRides.filter(function (ride) {
             if (JSON.stringify(ride.ride_end_time).length === 1) {
                 let currentFlag = Date.parse(`${ride.ride_date} 00:0${JSON.stringify(ride.ride_end_time)}:00:00`) - Date.now()
                 return currentFlag <= 0
             } else {
                 let currentFlag = Date.parse(`${ride.ride_date} 00:${JSON.stringify(ride.ride_end_time)}:00:00`) - Date.now()
                 return currentFlag <= 0
-            }})
-            .map( ride => (
+            }
+        })
+            .map(ride => (
                 <DriverPastRides key={ride.ride_id} {...ride} />
             ))
-        return(
-        
-            <div>
-                <header>
-                    <button onClick={this.goBack}>{`<Back`}</button>
-                    <h1>This view will list all of the past user trips and will allow riders to rate the driver</h1>
-                    <div>{ridesDisplay}</div>
-                    <button onClick={this.goToSearchPage}>Search for a New Ride</button> 
-                </header>
+        return (
+
+
+            <div className="mainAppWindow">
+                <section className="normalPageContainer">
+                    <section className="normalPageWhiteBox">
+                        <h2 className="mapPageContainerHeader">RIDE HISTORY</h2>
+                        <div className="availableRideContainer" style={{ backgroundColor: "white", height: "5vh" }}>
+                            <div className="six wide column" style={{ marginLeft: "4%" }}>DATE</div>
+                            <div className="one wide column" style={{ marginLeft: "7%" }}></div>
+                            <div className="one wide column" style={{ marginLeft: "16%" }}>TIER</div>
+                            <div className="eight wide column" style={{ overflowX: "auto", marginLeft: "5%" }}>LOCATION</div>
+                        </div>
+                        <div className="allAvailRidesContainer" >{
+                            filteredRides.filter(function (ride) {
+                                if (JSON.stringify(ride.ride_end_time).length === 1) {
+                                    let currentFlag = Date.parse(`${ride.ride_date} 00:0${JSON.stringify(ride.ride_end_time)}:00:00`) - Date.now()
+                                    return currentFlag <= 0
+                                } else {
+                                    let currentFlag = Date.parse(`${ride.ride_date} 00:${JSON.stringify(ride.ride_end_time)}:00:00`) - Date.now()
+                                    return currentFlag <= 0
+                                }
+                            })
+                                .map(ride => (
+                                    <DriverPastRides key={ride.ride_id} {...ride} eventTypes={["click"]} />
+                                ))
+                            // < div > {ridesDisplay}</div>
+                            // <button onClick={this.goToSearchPage}>Search for a New Ride</button>
+                        }</div>
+                    </section>
+                </section>
             </div>
         );
     };
 }
 
-function mapStateToProps(state){
-    return state
-  }
 
-  export default connect(mapStateToProps, {getRidesByDriverId})(withRouter(DriverHistory));
+function mapStateToProps(state) {
+    return state
+}
+
+export default connect(mapStateToProps, { getRidesByDriverId })(withRouter(DriverHistory));
--- src/components/DriverPastRides.js
+++ src/components/DriverPastRides.js
@@ -1,41 +1,85 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Rating} from 'semantic-ui-react'
+import { Rating } from 'semantic-ui-react'
+import "./coreStyling.css";
+import "./dashboardStyling.css";
+import onClickOutside from 'react-onclickoutside';
 
 class DriverPastRides extends Component {
-    state = {
-        rating: 0,
-        maxRating: 5
+    constructor(props) {
+        super(props)
+        this.state = {
+            rating: 0,
+            maxRating: 5,
+            selected: false
+        }
     }
-    
-    handleRate = (e, { rating, maxRating }) => this.setState({ rating, maxRating }) 
-        
-    
-        
+
+
+    handleRate = (e, { rating, maxRating }) => this.setState({ rating, maxRating })
+
+
+    clickedRide = () => {
+        let currentSelectedState = this.state.selected;
+        this.setState({ selected: !currentSelectedState })
+    }
+
+    handleClickOutside = () => {
+        if (this.state.selected === true) {
+            this.clickedRide();
+        }
+
+    }
+
     buyRide = () => {
         console.log('Buy a ride button triggered!')
     }
 
     render() {
         let { ride_id, rider_first_name, rider_last_name, ride_date, ride_location, ride_open_seats, ride_start_time, ride_end_time, tier_id } = this.props;
-        console.log(this.state.rating, 'rating')
-        return (
-        <div style={{backgroundColor: 'yellow', margin: '10px'}}>
-            <p>{ride_id}</p>
-            <p>Date: {ride_date}</p>
-            <p>Location: {ride_location}</p>
-            <p>Seats: {ride_open_seats}</p>
-            <p>Start Time: {ride_start_time}</p>
-            <p>End Time: {ride_end_time}</p>
-            <p>Rider: {rider_first_name} {rider_last_name}</p>
-            <p>Tier: {tier_id}</p>
-            <p>Please rate the trip.</p>
-            <div>
-                <Rating icon='star' maxRating={5} onRate={this.handleRate} /> 
-            </div>
-        </div>
-        );
+
+        if (this.state.selected === false) {
+            return (
+
+                <section className="availableRideContainer" onClick={this.clickedRide}>
+                    <div className="six wide column" style={{ marginLeft: "4%" }}>{ride_date}</div>
+                    <div className="one wide column" style={{ marginLeft: "6%" }}>{ride_open_seats}</div>
+                    <div className="one wide column" style={{ marginLeft: "13%" }}>{tier_id}</div>
+                    <div className="eight wide column" style={{ overflowX: "auto", marginLeft: "9%" }}>{ride_location}</div>
+                </section>
+            );
+        } else {
+            return (
+
+                <section className="selectedRideContainer" style={{ height: "30vh" }} onClick={this.clickedRide}>
+                    <div className="selectedRideWhiteBox">
+                        <div className="selectedRideRowContainer">
+                            <div className="selectedRideParagraph">Ride Number: {ride_id}</div>
+                        </div>
+                        <div className="selectedRideRowContainer">
+                            <div className="selectedRideParagraph">Location: {ride_location}</div>
+                        </div>
+                        <div className="selectedRideRowContainer">
+                            <div className="selectedRideParagraph" >Rider: {rider_first_name} {rider_last_name}</div>
+                            <div className="selectedRideParagraph">Ride Date: {ride_date}</div>
+                            <div className="selectedRideParagraph">Seats: {ride_open_seats}</div>
+                        </div>
+                        <div className="selectedRideRowContainer" style={{ marginTop: "8%" }}>
+                            <div className="selectedRideParagraph">Tier: {tier_id}</div>
+                            <div className="selectedRideParagraph">Start Time: {ride_start_time}</div>
+                            <div className="selectedRideParagraph">End Time: {ride_end_time}</div>
+                        </div>
+                        <div className="selectedRideRowContainer" style={{ marginBottom: "5%" }}>
+                            <div className="selectedRideParagraph" style={{ marginTop: "10%" }}>Please rate the trip </div>
+                        </div>
+                        <div className="selectedRideRowContainer" style={{ marginBottom: "5%" }}>
+                            <Rating icon='star' maxRating={5} onRate={this.handleRate} />
+                        </div>
+                    </div>
+                </section>
+            );
+        }
     }
 }
 
-export default connect( null, null)(DriverPastRides);
+export default connect(null, null)(onClickOutside(DriverPastRides))
