53feddf255e1ba70234268a71531fe86fe90a58f
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-08-28 23:24:20 -0600

    Updated Menu to Include Unfilled Rides for Drivers

--- src/components/DriverCreatedRide.js
+++ src/components/DriverCreatedRide.js
@@ -7,19 +7,17 @@ class DriverPastRides extends Component {
 
 
     render() {
-        let { ride_id, rider_id, ride_date, ride_location, ride_open_seats, ride_start_time, ride_end_time, tier_id } = this.props;
+        let { ride_id, ride_date, ride_location, ride_open_seats, ride_start_time, ride_end_time, tier_id } = this.props;
         
         return (
         <div style={{backgroundColor: 'yellow', margin: '10px'}}>
             <p>{ride_id}</p>
             <p>Date: {ride_date}</p>
             <p>Location: {ride_location}</p>
-            <p>Seats: {ride_open_seats}</p>
             <p>Start Time: {ride_start_time}</p>
             <p>End Time: {ride_end_time}</p>
-            <p>Rider: {rider_id}</p>
+            <p>Seats: {ride_open_seats}</p>
             <p>Tier: {tier_id}</p>
-            <p>Pricing</p>
         </div>
         );
     }
